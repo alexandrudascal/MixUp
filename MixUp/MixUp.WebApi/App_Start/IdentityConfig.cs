fa63984fc7d4a1169af7fab9b755de892191801a
Alexandru Dascal <dascalalexandru@gmail.com>
2016-08-19 15:02:32 +0300

    Added UserManager configuration in WebApi project

---
+++ MixUp/MixUp.WebApi/App_Start/IdentityConfig.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using MixUp.DataAccess;
+using MixUp.Infrastructure;
+using Owin;
+
+namespace MixUp.WebApi.App_Start
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(MixUpDbContext.Create);
+            app.CreatePerOwinContext<MixUpUserManager>(MixUpUserManager.Create);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+    }
+}
