659753439f98e2aaf4d6ea3771a0ae2fed630afb
Alexandru Dascal <dascalalexandru@gmail.com>
2016-08-19 13:29:14 +0300

    Added MixUp.WebApi project

---
+++ MixUp.WebApi/MixUp.WebApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MixUp.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ MixUp.WebApi/MixUp.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MixUp.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
