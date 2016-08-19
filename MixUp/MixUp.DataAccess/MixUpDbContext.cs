45f8a58a0aa5594c18f5eba2cc808cb7e679938e
Alexandru Dascal <dascalalexandru@gmail.com>
2016-08-19 14:38:18 +0300

    Created the basic of MixUpDbContext

---
+++ MixUp/MixUp.DataAccess/MixUpDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using MixUp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixUp.DataAccess
+{
+    public class MixUpDbContext : IdentityDbContext<User>
+    {
+        public MixUpDbContext()
+            :base("MixUpContext")
+        {
+        }
+
+        static MixUpDbContext()
+        {
+            Database.SetInitializer(new MixUpDbInitializer());
+        }
+        public static MixUpDbContext Create()
+        {
+            return new MixUpDbContext();
+        }
+    }
+}
---
+++ MixUp/MixUp.DataAccess/MixUpDbInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixUp.DataAccess
+{
+    class MixUpDbInitializer: DropCreateDatabaseIfModelChanges<MixUpDbContext>
+    {
+        protected override void Seed(MixUpDbContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
