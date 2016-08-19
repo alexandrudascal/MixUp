d589652a6a48cba394cf2bafc8fac6cb5caab182
Alexandru Dascal <dascalalexandru@gmail.com>
2016-08-19 16:46:20 +0300

    added MixUpRole class & UserRoles enum

---
+++ MixUp/MixUp.Entities/MixUpRole.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixUp.Entities
+{
+    public class MixUpRole : IdentityRole
+    {
+        public MixUpRole() : base() { }
+        public MixUpRole(string name) : base(name) { }
+    }
+}
